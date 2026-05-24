e8768b019e30b713941934b6eada7011978836bc
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-05-24 18:07:12 +0530

    Update solution.py

--- Python-PALB/row-with-max-1s-in-rowwise-sorted/solution.py
+++ Python-PALB/row-with-max-1s-in-rowwise-sorted/solution.py
@@ -1,1 +1,15 @@
+class Solution:
+    def rowWithMax1s(self, arr):
 
+        max_ones = 0
+        index = -1
+
+        for i in range(len(arr)):
+
+            count = arr[i].count(1)
+
+            if count > max_ones:
+                max_ones = count
+                index = i
+
+        return index

5d53e8e36207e1e49be5cc411377aca29354b768
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-05-24 18:46:14 +0530

    Update solution.py

--- Python-PALB/union-of-array/solution.py
+++ Python-PALB/union-of-array/solution.py
@@ -1,1 +1,13 @@
+class Solution:
 
+    def findUnion(self, a, b):
+
+        union_set = set()
+
+        for num in a:
+            union_set.add(num)
+
+        for num in b:
+            union_set.add(num)
+
+        return list(union_set)

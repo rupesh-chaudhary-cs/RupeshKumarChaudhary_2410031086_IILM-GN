994d92771ba98410fbcb0ffbbbbd7f39d4236a07
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-05-24 12:07:55 +0530

    Update solution.py

--- Python-PALB/check-if-permutaion-is-substring/solution.py
+++ Python-PALB/check-if-permutaion-is-substring/solution.py
@@ -1,1 +1,27 @@
+class Solution:
+    def search(self, txt, pat):
 
+        n = len(txt)
+        m = len(pat)
+
+        freq1 = [0] * 26
+        freq2 = [0] * 26
+
+        for ch in pat:
+            freq1[ord(ch) - ord('a')] += 1
+
+        for i in range(m):
+            freq2[ord(txt[i]) - ord('a')] += 1
+
+        if freq1 == freq2:
+            return True
+
+        for i in range(m, n):
+
+            freq2[ord(txt[i]) - ord('a')] += 1
+            freq2[ord(txt[i - m]) - ord('a')] -= 1
+
+            if freq1 == freq2:
+                return True
+
+        return False

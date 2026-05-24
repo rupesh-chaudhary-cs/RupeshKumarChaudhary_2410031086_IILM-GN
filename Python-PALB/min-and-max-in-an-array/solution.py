1449205bd875c848da05654cc7456e462c5929f3
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-05-24 18:31:53 +0530

    Update solution.py

--- Python-PALB/min-and-max-in-an-array/solution.py
+++ Python-PALB/min-and-max-in-an-array/solution.py
@@ -1,1 +1,15 @@
+class Solution:
+    def getMinMax(self, arr):
 
+        min_arr = arr[0]
+        max_arr = arr[0]
+
+        for i in range(len(arr)):
+
+            if min_arr > arr[i]:
+                min_arr = arr[i]
+
+            if max_arr < arr[i]:
+                max_arr = arr[i]
+
+        return min_arr, max_arr

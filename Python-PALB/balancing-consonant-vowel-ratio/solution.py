84f04602be6ef48d30ac4f5d3cb2aff65f822745
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-05-24 18:53:04 +0530

    Update solution.py

--- Python-PALB/balancing-consonant-vowel-ratio/solution.py
+++ Python-PALB/balancing-consonant-vowel-ratio/solution.py
@@ -1,1 +1,29 @@
-
+class Solution:
+    
+    def countBalanced(self, arr):
+        
+        vowels = set("aeiou")
+        
+        prefix = 0
+        freq = {0: 1}
+        ans = 0
+        
+        for s in arr:
+            
+            v = 0
+            c = 0
+            
+            for ch in s:
+                
+                if ch in vowels:
+                    v += 1
+                else:
+                    c += 1
+            
+            prefix += (v - c)
+            
+            ans += freq.get(prefix, 0)
+            
+            freq[prefix] = freq.get(prefix, 0) + 1
+        
+        return ans

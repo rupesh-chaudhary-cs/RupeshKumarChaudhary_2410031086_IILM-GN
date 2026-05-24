8f215c1965f48a4b90baefa6b82d654b3ae4bece
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-05-24 12:03:27 +0530

    Update solution.py

--- Python-PALB/count-pair-of-string-with-one-mismatch/solution.py
+++ Python-PALB/count-pair-of-string-with-one-mismatch/solution.py
@@ -1,1 +1,19 @@
+class Solution:
+    def countPairs(self, arr):
 
+        mp = {}
+        ans = 0
+        n = len(arr)
+        m = len(arr[0])
+
+        for s in arr:
+
+            for i in range(m):
+
+                pattern = s[:i] + "*" + s[i+1:]
+
+                ans += mp.get(pattern, 0)
+
+                mp[pattern] = mp.get(pattern, 0) + 1
+
+        return ans

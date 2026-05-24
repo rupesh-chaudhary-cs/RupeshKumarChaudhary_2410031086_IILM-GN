10b3b66cd35075d42277ced2deffaa3e3091ac8a
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-05-24 11:56:28 +0530

    Update solution.py

--- Python-PALB/difference-check/solution.py
+++ Python-PALB/difference-check/solution.py
@@ -1,1 +1,28 @@
+class Solution:
 
+    def toSeconds(self, t):
+        h = int(t[0:2])
+        m = int(t[3:5])
+        s = int(t[6:8])
+
+        return h * 3600 + m * 60 + s
+
+    def minDifference(self, arr):
+
+        times = []
+
+        for t in arr:
+            times.append(self.toSeconds(t))
+
+        times.sort()
+
+        ans = float('inf')
+
+        for i in range(1, len(times)):
+            ans = min(ans, times[i] - times[i - 1])
+
+        wrap = 86400 - times[-1] + times[0]
+
+        ans = min(ans, wrap)
+
+        return ans

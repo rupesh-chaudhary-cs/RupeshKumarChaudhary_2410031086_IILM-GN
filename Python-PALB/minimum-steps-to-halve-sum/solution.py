da2fd094a6e499c0af22b8af2d39e9a6aaf25453
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-05-24 18:02:53 +0530

    Update solution.py

--- Python-PALB/minimum-steps-to-halve-sum/solution.py
+++ Python-PALB/minimum-steps-to-halve-sum/solution.py
@@ -1,1 +1,27 @@
+import heapq
 
+class Solution:
+    def minOperations(self, arr):
+
+        total = sum(arr)
+        target = total / 2
+
+        max_heap = [-x for x in arr]
+        heapq.heapify(max_heap)
+
+        operations = 0
+        current = total
+
+        while current > target:
+
+            largest = -heapq.heappop(max_heap)
+
+            half = largest / 2
+
+            current -= half
+
+            heapq.heappush(max_heap, -half)
+
+            operations += 1
+
+        return operations

8b50cf8961ae33edc19709121619df4187185c1a
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-05-24 18:36:15 +0530

    Update solution.py

--- Python-PALB/chocolate-distribution-problem/solution.py
+++ Python-PALB/chocolate-distribution-problem/solution.py
@@ -1,1 +1,19 @@
+class Solution:
+    def findMinDiff(self, arr, m):
 
+        n = len(arr)
+
+        if m == 0 or n == 0:
+            return 0
+
+        arr.sort()
+
+        minimum = float('inf')
+
+        for i in range(n - m + 1):
+
+            diff = arr[i + m - 1] - arr[i]
+
+            minimum = min(minimum, diff)
+
+        return minimum

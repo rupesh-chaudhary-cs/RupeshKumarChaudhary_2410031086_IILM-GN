6bd74857bb5c7aebf1faf681b896918bb1b82c8e
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-05-24 12:05:28 +0530

    Update solution.py

--- Python-PALB/winner-of-an-election/solution.py
+++ Python-PALB/winner-of-an-election/solution.py
@@ -1,1 +1,18 @@
+class Solution:
+    def winner(self, arr):
 
+        freq = {}
+
+        for name in arr:
+            freq[name] = freq.get(name, 0) + 1
+
+        max_votes = max(freq.values())
+
+        ans = ""
+
+        for name in freq:
+            if freq[name] == max_votes:
+                if ans == "" or name < ans:
+                    ans = name
+
+        return [ans, str(max_votes)]

09dc1202979785393249130ea099cfa21e1716a7
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-05-24 11:53:58 +0530

    Update solution.py

--- Python-PALB/score-of-parentless-string/solution.py
+++ Python-PALB/score-of-parentless-string/solution.py
@@ -1,1 +1,14 @@
+class Solution:
+    def scoreOfParentheses(self, s):
 
+        stack = [0]
+
+        for ch in s:
+
+            if ch == '(':
+                stack.append(0)
+            else:
+                val = stack.pop()
+                stack[-1] += max(2 * val, 1)
+
+        return stack[0]

c75f53c4307fb732b44b415d6e458bc9ddb36d15
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-05-24 12:10:17 +0530

    Update solution.py

--- Python-PALB/lexicographically-largest-string-after-k-deletions/solution.py
+++ Python-PALB/lexicographically-largest-string-after-k-deletions/solution.py
@@ -1,1 +1,19 @@
+class Solution:
+    def maxSubseq(self, s, k):
 
+        remove = k
+        stack = []
+
+        for ch in s:
+
+            while stack and remove > 0 and stack[-1] < ch:
+                stack.pop()
+                remove -= 1
+
+            stack.append(ch)
+
+        while remove > 0:
+            stack.pop()
+            remove -= 1
+
+        return "".join(stack)

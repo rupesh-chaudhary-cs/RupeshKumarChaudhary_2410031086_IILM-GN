7767331ebd31bc1202f4d611f14153d69a98474b
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-05-24 12:15:40 +0530

    Update solution.py

--- Python-PALB/count-even-letters/solution.py
+++ Python-PALB/count-even-letters/solution.py
@@ -1,1 +1,15 @@
+class Solution:
+    def count(self, s):
 
+        freq = {}
+
+        for ch in s:
+            freq[ch] = freq.get(ch, 0) + 1
+
+        ans = 0
+
+        for val in freq.values():
+            if val % 2 == 0:
+                ans += 1
+
+        return ans

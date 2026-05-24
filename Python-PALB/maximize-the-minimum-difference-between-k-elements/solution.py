a290aa5d0116b1c691bae650c8e8836e77015253
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-05-24 11:48:19 +0530

    Update solution.py

--- Python-PALB/maximize-the-minimum-difference-between-k-elements/solution.py
+++ Python-PALB/maximize-the-minimum-difference-between-k-elements/solution.py
@@ -1,1 +1,38 @@
+class Solution:
 
+    def isPossible(self, arr, k, mid):
+
+        count = 1
+        last = arr[0]
+
+        for i in range(1, len(arr)):
+
+            if arr[i] - last >= mid:
+                count += 1
+                last = arr[i]
+
+            if count >= k:
+                return True
+
+        return False
+
+    def maxMinDiff(self, arr, k):
+
+        arr.sort()
+
+        low = 0
+        high = arr[-1] - arr[0]
+
+        ans = 0
+
+        while low <= high:
+
+            mid = (low + high) // 2
+
+            if self.isPossible(arr, k, mid):
+                ans = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return ans

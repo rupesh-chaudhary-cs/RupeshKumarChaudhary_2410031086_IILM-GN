4e0c358a4697fc5485e70d3e21baea77233ad1bd
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-05-24 18:24:04 +0530

    Update solution.py

--- Python-PALB/common-in-3-sorted-array/solution.py
+++ Python-PALB/common-in-3-sorted-array/solution.py
@@ -1,1 +1,27 @@
+class Solution:
+    def commonElements(self, a, b, c):
 
+        i = j = k = 0
+        result = []
+
+        while i < len(a) and j < len(b) and k < len(c):
+
+            if a[i] == b[j] == c[k]:
+
+                if not result or result[-1] != a[i]:
+                    result.append(a[i])
+
+                i += 1
+                j += 1
+                k += 1
+
+            elif a[i] < b[j]:
+                i += 1
+
+            elif b[j] < c[k]:
+                j += 1
+
+            else:
+                k += 1
+
+        return result

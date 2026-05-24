26d451d8d58f957d2b1752497fb9ac9977f717fc
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-05-24 11:59:20 +0530

    Update solution.py

--- Python-PALB/sort-by-frequency/solution.py
+++ Python-PALB/sort-by-frequency/solution.py
@@ -1,1 +1,16 @@
+class Solution:
+    def frequencySort(self, s):
 
+        freq = {}
+
+        for ch in s:
+            freq[ch] = freq.get(ch, 0) + 1
+
+        arr = sorted(freq.items(), key=lambda x: (x[1], x[0]))
+
+        ans = ""
+
+        for ch, cnt in arr:
+            ans += ch * cnt
+
+        return ans

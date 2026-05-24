048ed4875789b3c55d37becea1793ba4b875736c
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-05-24 18:11:24 +0530

    Update solution.py

--- Python-PALB/median-in-a-row-wise-sorted-matrix/solution.py
+++ Python-PALB/median-in-a-row-wise-sorted-matrix/solution.py
@@ -1,1 +1,14 @@
+class Solution:
+    def median(self, mat):
 
+        n = len(mat)
+        m = len(mat[0])
+
+        arr = []
+
+        for row in mat:
+            arr.extend(row)
+
+        arr.sort()
+
+        return arr[(n * m) // 2]

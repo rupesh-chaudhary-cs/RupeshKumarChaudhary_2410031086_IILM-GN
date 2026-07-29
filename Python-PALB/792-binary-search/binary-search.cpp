ced04ab10dcea1b58a85695b8996e8ad04f7f9ed
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-07-29 18:00:03 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (8.10%) - LeetSync

---
+++ Python-PALB/792-binary-search/binary-search.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+
+    int binarySearch(vector<int>& nums,int target,int low,int high){
+            int mid=(low+high)/2;
+            if(low>high){
+                return -1;
+            }
+            if(target==nums[mid]){
+                return mid;
+            }else if(target>nums[mid]){
+                return binarySearch(nums,target,mid+1,high);
+            }else if(target<nums[mid]){
+                return binarySearch(nums,target,low,mid-1);
+            }
+            return -1;
+        }
+    int search(vector<int>& nums, int target) {
+        return binarySearch(nums,target,0,nums.size()-1);
+    }
+
+};

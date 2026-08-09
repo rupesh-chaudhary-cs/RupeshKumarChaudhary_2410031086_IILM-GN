5413bf4ae0160f7b5695086e81a8c3117524361c
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-08-09 18:24:52 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (71.45%) - LeetSync

--- Python-PALB/268-missing-number/missing-number.cpp
+++ Python-PALB/268-missing-number/missing-number.cpp
@@ -4,12 +4,15 @@ public:
         int N=nums.size();
         int requiredSum=0;
         int currentSum=0;
+        int j=0;
         for(int i=0;i<=N;i++){
             requiredSum+=i;
+            if(i<N){
+                currentSum+=nums[j];
+                j++;
+            }
         }
-        for(int j=0;j<N;j++){
-            currentSum+=nums[j];
-        }
+        
         if(requiredSum>currentSum){
             return requiredSum-currentSum;
             

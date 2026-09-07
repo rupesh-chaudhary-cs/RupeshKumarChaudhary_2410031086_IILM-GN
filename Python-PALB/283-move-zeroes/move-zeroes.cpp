84f30d78bb7f2fef4e11f2539b1742c4425d297c
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-09-07 22:29:16 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (82.72%) - LeetSync

--- Python-PALB/283-move-zeroes/move-zeroes.cpp
+++ Python-PALB/283-move-zeroes/move-zeroes.cpp
@@ -2,23 +2,12 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int N=nums.size();
-        vector<int>v;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                v.push_back(nums[i]);
-            }
-        }
         int j=0;
         for(int i=0;i<N;i++){
-            if(nums[i]>0 || nums[i]<0){
-                nums[j]=nums[i];
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
                 j++;
             }
-        }
-        int l=1;
-        for(int j=0;j<v.size();j++){
-            nums[N-l]=v[j];
-            l++;
         }
     }
 };

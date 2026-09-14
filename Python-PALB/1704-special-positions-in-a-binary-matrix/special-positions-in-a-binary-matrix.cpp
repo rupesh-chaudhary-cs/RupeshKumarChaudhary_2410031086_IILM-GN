0611d64e96709ac4f26aa1a3e53fa02107bedc46
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-09-14 18:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (81.50%) - LeetSync

--- Python-PALB/1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ Python-PALB/1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -1,33 +1,44 @@
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
+       int result=0;
+       int count=0;
+       int count1=0;
        int m=mat.size();
        int n=mat[0].size();
-       int count=0;
-       int secondCount=0;
-       for(int i=0;i<m;i++) {
-         for(int j=0;j<n;j++){
-            if (mat[i][j]==1){
-                count++;
-                for(int k=0;k<n;k++){
-                    if(mat[i][k]==1){
-                        count++;
+       for(int i=0;i<m;i++){
+           for(int j=0;j<n;j++){
+                if(mat[i][j]==1){
+                    for(int k=0;k<n;k++){
+                        if(mat[i][k]==1){
+                            count++;
+                        }
                     }
-                }
-                for(int l=0;l<m;l++){
-                    if(mat[l][j]==1){
-                        count++;
+                    if(count!=1){
+                        count=0;
+                        count1=0;
+                        break;
                     }
+                    for(int l=0;l<m;l++){
+                        if(mat[l][j]==1){
+                            count1++;
+                        }
+                    }
+                    if(count1!=1){
+                        count1=0;
+                        count=0;
+                        break;
+                    }
+
                 }
-            }
-            if(count==3){
-                secondCount++;
-            }
-            count=0;
-            
-         }
-        }
-        return secondCount;
+                if(count==1 && count1==1){
+                    result++;
+                }
+                count=0;
+                count1=0;
+           }
+       }
+       return result;
 
     }
 };

4c7cef21c765dd39abc95c1a574cc8eb1a45a477
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-09-14 17:39:45 +0530

    Time: 0 ms (100.00%) | Memory: 21.1 MB (26.54%) - LeetSync

--- Python-PALB/777-toeplitz-matrix/toeplitz-matrix.cpp
+++ Python-PALB/777-toeplitz-matrix/toeplitz-matrix.cpp
@@ -5,7 +5,6 @@ public:
         int n=matrix[0].size();
         for(int i=0;i<m-1;i++){
             for(int j=0;j<n-1;j++){
-            
                 if(matrix[i][j]!=matrix[i+1][j+1]){
                     return false;
                 }
